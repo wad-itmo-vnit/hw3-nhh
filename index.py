5c365115de3b7bf07b89a7f78b49e189e8b3a03c
hanhnguyen26 <NHHanh.2610@gmail.com>
2021-08-06 14:59:21 +0300

    updated

--- index.py
+++ index.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/img/<filename>.jpg')
+def sendImg(filename):
+    return send_file('./img/'+ str(filename) + '.jpg')
 
 app.run(host="localhost", port=5000)
